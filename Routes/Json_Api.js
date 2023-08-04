babdb1d62e6aacd1c422af317f90c87fba4d5def
Anant Prajapati <113686236+Anant-83@users.noreply.github.com>
2023-08-04 22:49:43 +0530

    Update Json_Api.js

--- Routes/Json_Api.js
+++ Routes/Json_Api.js
@@ -5,9 +5,12 @@ const User = require('../Database/mongo');
 router.post('/', async (req, res) => {
     console.log(req.body);
     const check = await User.findOne({ Product_Name: req.body.Product_Name });
-    if (check) {
+    if (check)
+    {
         res.send("Data of this Product is already present in our database...");
-    } else {
+    } 
+    else {
+        
         const addUser = User(req.body);
         await addUser.save();
         res.send("Data added successfully!!");
